4a6d89ec827ed4f3bea7242e1e9299d246410896
platzhersh <chregi.glatthard@gmail.com>
2015-01-02 09:44:45 +0100

    primitive first version working

--- swissrugbystats.js
+++ swissrugbystats.js
@@ -6,14 +6,37 @@ srsApp.config(function ($routeProvider){
 	$routeProvider
 		.when('/',
 		{
-			redirectTo: '/team/42'
-			//controller : 'SimpleController',
-			//templateUrl : 'views/view1.htm'
+			redirectTo: '/leagues'
+		})
+		.when('/leagues',
+		{
+			controller : 'LeagueController',
+			templateUrl : 'views/leagues.html'
+		})
+		.when('/venues',
+		{
+			controller : 'VenueController',
+			templateUrl : 'views/venues.html'
+		})
+		.when('/games',
+		{
+			controller : 'SwissRugbyStatsController',
+			templateUrl : 'views/games.html'
+		})
+		.when('/teams',
+		{
+			controller : 'SwissRugbyStatsController',
+			templateUrl : 'views/teams.html'
 		})
 		.when('/team/:teamId',
 		{
 			controller : 'SwissRugbyStatsController',
-			templateUrl : 'views/team.htm'
+			templateUrl : 'views/team.html'
+		})
+		.when('/referees',
+		{
+			controller : 'RefereeController',
+			templateUrl : 'views/referees.html'
 		})
 		.otherwise({ redirectTo: '/' });
 });
@@ -37,6 +60,7 @@ function SwissRugbyStatsController($scope, $routeParams, $filter, $http) {
         $scope.teams = data;
     });
 
+    // custom filters
 	$scope.gameParticipantFilter = function (game) {
 		if ($scope.team1 && $scope.team2) {
 	    	return ((game.host.team.id == $scope.team1.id ||
@@ -50,6 +74,39 @@ function SwissRugbyStatsController($scope, $routeParams, $filter, $http) {
 	    	return $scope.games;
 	    }
 	}
+	$scope.exactMatch = function (team) {
+		return team.id == $scope.teamId;
+	}
+}
+
+function RefereeController($scope, $routeParams, $filter, $http) {
+	
+	$scope.referees = {};
+	$http.get(apiurl+'/referees/.json').
+        success(function(data) {
+            $scope.referees = data;
+    });
+}
+
+function LeagueController($scope, $routeParams, $filter, $http) {
+	
+	$scope.leagues = {};
+	$http.get(apiurl+'/leagues/.json').
+        success(function(data) {
+            $scope.leagues = data;
+    });
+}
+
+function VenueController($scope, $routeParams, $filter, $http) {
+	
+	$scope.venues = {};
+	$http.get(apiurl+'/venues/.json').
+        success(function(data) {
+            $scope.venues = data;
+    });
 }
 
 srsApp.controller('SwissRugbyStatsController', SwissRugbyStatsController);
+srsApp.controller('RefereeController', RefereeController);
+srsApp.controller('LeagueController', LeagueController);
+srsApp.controller('VenueController', VenueController);
